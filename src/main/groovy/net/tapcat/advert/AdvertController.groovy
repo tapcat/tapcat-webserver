88a04936a6e06cf0527b07bbc2866d2dc690b77b
OctaDecimal <borov.htid@gmail.com>
2013-09-26 15:05:49 +0400

    Switch to RestController

--- src/main/groovy/net/tapcat/advert/AdvertController.groovy
+++ src/main/groovy/net/tapcat/advert/AdvertController.groovy
@@ -1,11 +1,11 @@
 package net.tapcat.advert
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping('/ad')
 class AdvertController {
 

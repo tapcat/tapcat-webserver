eb62bafef28ebe9978d3cc231573631fed02495d
WonderBeat <borov.htid@gmail.com>
2013-10-02 16:57:08 +0400

    Cors updated

--- src/main/groovy/net/tapcat/security/CorsFilter.groovy
+++ src/main/groovy/net/tapcat/security/CorsFilter.groovy
@@ -13,7 +13,7 @@ class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader('Access-Control-Allow-Origin', '//localhost')
+        response.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000')
         filterChain.doFilter(request, response)
     }
 }

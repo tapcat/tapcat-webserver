9c4164ed3f17eb00f6d7d0f6a6977ed96c0b31fd
WonderBeat <borov.htid@gmail.com>
2013-10-04 18:06:58 +0400

    Host header will contain request url info when we are behind NGINX. fix

--- src/main/groovy/persona/BrowserIdProcessingFilter.groovy
+++ src/main/groovy/persona/BrowserIdProcessingFilter.groovy
@@ -34,7 +34,7 @@ class BrowserIdProcessingFilter  extends AbstractAuthenticationProcessingFilter 
         if(!browserIdAssertion || !HttpMethod.POST.name().equals(request.getMethod())) {
             throw new BrowserIdAuthenticationException('Authentication request should contain assertion')
         }
-        def requestUrl = request.getHeader('Host')?: request.getRequestURL() // proxy pass will rewrite request url.
+        def requestUrl = request.getHeader('Host')?: request.getRequestURL().toString() // proxy pass will rewrite request url.
         // can restore it from Host header
         authenticate(browserIdAssertion, requestUrl)
     }

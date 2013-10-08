34f3a079806cbdf448b78e21c08f305d44cd4bc0
OctaDecimal <borov.htid@gmail.com>
2013-10-08 13:43:07 +0400

    JACKSON updated to 2 major version

--- src/main/groovy/net/tapcat/config/RestConfig.groovy
+++ src/main/groovy/net/tapcat/config/RestConfig.groovy
@@ -1,12 +1,13 @@
 package net.tapcat.config
-import org.codehaus.jackson.map.DeserializationConfig
-import org.codehaus.jackson.map.ObjectMapper
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.http.converter.HttpMessageConverter
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 
 @Configuration
@@ -18,21 +19,19 @@ class RestConfig {
     @Bean
     ObjectMapper objectMapper() {
         def mapper = new ObjectMapper()
-        mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
 
     @Bean
     HttpMessageConverter httpMessageConverter(ObjectMapper mapper) {
-        def converter = new MappingJacksonHttpMessageConverter()
+        def converter = new MappingJackson2HttpMessageConverter()
         converter.setObjectMapper(mapper)
         converter
     }
 
     @Bean
     public RestTemplate httpFactory() {
-        def connectionFactory = new SimpleClientHttpRequestFactory()
-        connectionFactory.setReadTimeout(500)
-        connectionFactory.setConnectTimeout(500)
+        def connectionFactory = new SimpleClientHttpRequestFactory(readTimeout: 1000, connectTimeout: 1000)
         def restTemplate = new RestTemplate(connectionFactory)
         restTemplate.messageConverters = [httpMessageConverter] as List
         restTemplate

83073bd82caccb533eac90dd428c8f3cb06bb0e8
WonderBeat <borov.htid@gmail.com>
2013-10-15 17:14:02 +0400

    500 for dev environment. Heh. It's not extractor bug

--- src/test/groovy/net/tapcat/security/TopDomainAudienceResolverTest.groovy
+++ src/test/groovy/net/tapcat/security/TopDomainAudienceResolverTest.groovy
@@ -17,6 +17,7 @@ class TopDomainAudienceResolverTest extends Specification {
         'tapcat.net:8080'|'tapcat.net:8080'
         'http://ya.ru'  | 'ya.ru'
         'cat.tapcat.net'| 'tapcat.net'
+        'dev.tapcat.net:9000' | 'tapcat.net:9000'
         'cat.ya.ru:90'  | 'ya.ru:90'
     }
 
